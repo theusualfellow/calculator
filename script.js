7b7acc340cc436435e314b9eec4b9388d3bdd446
sandeep <sandeepsingh23ok@gmail.com>
2024-04-01 01:07:13 -0300

    Calculate before clicking next operation button

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ const output = document.querySelector(".inputNums")
 const ops = document.querySelectorAll(".ops")
 const del= document.querySelector(".delete")
 const ce = document.querySelector(".ce")
-const equal = document.querySelector("equal")
-let number=0
+let resultingNumber=0
 let num1=0
 let num2=0
+let num3=0
+let num4=0
 
-
-let buttonText = button.innerText
 buttons.forEach(button => {
     button.addEventListener("click", ()=>{
-        output.append(buttonText)
+        output.append(button.innerText)
+        num1=output.innerText
         
-        
-    }
-        
-     
+        console.log(num1)
+        num4=0
+        num4=Number(num3)+Number(num1)
+        console.log(num4)
+
+    }    
     )})
 
 del.addEventListener("click", ()=>{
@@ -31,17 +33,30 @@ ce.addEventListener("click", ()=>{
 })
 
 ops.forEach(opertaion=>{
-    opertaion.addEventListener("click",()=>{
-        
-        console.log(num1)
-        output.append(opertaion.innerText)
-        num2+=buttons.innerText
-        
-        
+                opertaion.addEventListener("click",()=>{
+                num2=num1
+                num3= result(opertaion.innerText)
+                num1=''
+                console.log(num4)
+                output.innerText=''
+                
+
     } )
 })
 
-equal.addEventListener("click", ()=>{
-    const result= Number(num1)+Number(num2)
-    output.innerText=result
-})
+function result(op){
+    if(op=="+"){
+        resultingNumber=Number(resultingNumber)+Number(num2)
+        return resultingNumber
+    }
+    if(op=="-"){
+        if(resultingNumber==0){
+            resultingNumber=Number(num2)-Number(resultingNumber)
+        }
+        else{
+            resultingNumber=Number(resultingNumber)-Number(num2)
+        }
+        
+    }
+    
+}
